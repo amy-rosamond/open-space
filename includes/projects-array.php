0e71153a2143a9470de14aaa7a47a566250cc53f
Amy Rosamond <amy@taylorbennett.co.uk>
2026-08-25 15:33:14 +0100

    update

--- includes/projects-array.php
+++ includes/projects-array.php
@@ -7,17 +7,17 @@
 $navBarProjects = array();
 
 $navBarProjects[1] = array(
-    "href" => '#doors',
+    "href" => '#refurbishmentChorleywood',
     "name" => 'Refurbishment, Chorleywood'
 );
 
 $navBarProjects[2] = array(
-    "href" => '#kitchens',
+    "href" => '#conversionChingford',
     "name" => 'Conversion, Chingford'
 );
 
 $navBarProjects[3] = array(
-    "href" => '#roofs',
+    "href" => '#gardenRoom',
     "name" => 'Garden Room, Little Chalfont'
 );
 
@@ -25,7 +25,7 @@ $navBarProjects[3] = array(
 $conversionsInfo = array();
 
 $conversionsInfo[1] = array(
-    'id' => 'conversions',
+    'id' => 'conversionChingford',
     'title' => 'Conversion, Chingford',
     'info' => 'Located in Chingford, this modern conversion project has transformed a single bungalow into three exquisite, three-bedroom townhouses. Each townhouse boasts a thoughtfully designed floor plan that maximizes space and natural light. The luxurious interiors feature high-end finishes, designer fixtures, and state-of-the-art appliances. With their prime location and exceptional craftsmanship, these townhouses offer a unique opportunity for discerning homeowners seeking both luxury and convenience in the heart of Chingford.'
 );
@@ -34,7 +34,7 @@ $conversionsInfo[1] = array(
 $refurbInfo = array();
 
 $refurbInfo[1] = array(
-    'id' => 'refurb',
+    'id' => 'refurbishmentChorleywood',
     'title' => 'Refurbishment, Chorleywood',
     'info' => 'Located in the charming town of Chorleywood, this bungalow has undergone a remarkable transformation through a high-end refurbishment, epitomizing elegance and modernity. The property’s stunning makeover includes a spacious rear extension, seamlessly blending contemporary design with timeless aesthetics. This expansive addition incorporates a beautifully crafted living space, flooded with natural light, creating an inviting atmosphere. The centrepiece of the extension is a meticulously designed high-spec kitchen, boasting top-of-the-line appliances, sleek countertops, and ample storage. Every detail has been carefully considered, resulting in a truly exceptional living space that effortlessly combines style and functionality.'
 );
